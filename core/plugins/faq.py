4fb7bcca39bcb9819861d7133ecdc84a23424195
Christopher Glass <christopher_glass@dell.com>
2011-11-29 13:38:17 +0000

    Added a listfaq comand to the faq plugin
    For the time being it simply makes the bot spit out a list of available faq
    entries.

--- core/plugins/faq.py
+++ core/plugins/faq.py
@@ -35,6 +35,10 @@ class FAQ(BasePlugin):
                 self.faqs[identifier] = answer
                 self.write()
                 channel.msg('Added FAQ entry for %r' % identifier)
+
+    def command_listfaq(self, rest, user, channel):
+        for faq in self.faqs.keys():
+            channel.msg('%s' % faq)
     
     def command_faq(self, rest, user, channel):
         answer = self.faqs.get(rest, None)
